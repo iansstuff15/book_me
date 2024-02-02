e25ddc6dc1abd13ca1adc87b6ceb12384f563432
Diriangen Lempira Powell <dlpowell35@gmail.com>
2024-02-02 12:25:49 +0800

    chore: themes for dark and light config

---
+++ lib/config/themes/dark/dark-theme.config.dart
@@ -0,0 +1,7 @@
+import 'package:book_me/config/themes/dark/text.theme.dart';
+import 'package:flutter/material.dart';
+
+ThemeData darkAppTheme = ThemeData(
+  scaffoldBackgroundColor: Colors.black,
+  textTheme: textDarkTheme,
+);
---
+++ lib/config/themes/dark/text.theme.dart
@@ -0,0 +1,50 @@
+import 'package:book_me/config/font-weight.dart';
+import 'package:flutter/material.dart';
+
+TextTheme textDarkTheme = const TextTheme(
+  bodyLarge: TextStyle(
+    color: Colors.white,
+  ),
+  bodyMedium: TextStyle(
+    color: Colors.white,
+  ),
+  bodySmall: TextStyle(
+    color: Colors.white,
+  ),
+  displayLarge: TextStyle(
+    color: Colors.white,
+  ),
+  displayMedium: TextStyle(
+    color: Colors.white,
+  ),
+  displaySmall: TextStyle(
+    color: Colors.white,
+  ),
+  headlineLarge: TextStyle(
+    color: Colors.white,
+  ),
+  headlineMedium: TextStyle(
+    color: Colors.white,
+  ),
+  headlineSmall: TextStyle(
+    color: Colors.white,
+  ),
+  titleLarge: TextStyle(
+    color: Colors.white,
+  ),
+  titleMedium: TextStyle(
+    color: Colors.white,
+  ),
+  titleSmall: TextStyle(
+    color: Colors.white,
+  ),
+  labelLarge: TextStyle(
+    color: Colors.white,
+  ),
+  labelMedium: TextStyle(
+    color: Colors.white,
+  ),
+  labelSmall: TextStyle(
+    color: Colors.white,
+  ),
+);

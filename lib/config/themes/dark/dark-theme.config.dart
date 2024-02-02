cb8927a82046818b1e4590b148e40ced85163517
Diriangen Lempira Powell <dlpowell35@gmail.com>
2024-02-02 17:32:22 +0800

    feat: outline themedata updated

--- lib/config/themes/dark/dark-theme.config.dart
+++ lib/config/themes/dark/dark-theme.config.dart
@@ -4,4 +4,16 @@ import 'package:flutter/material.dart';
 ThemeData darkAppTheme = ThemeData(
   scaffoldBackgroundColor: Colors.black,
   textTheme: textDarkTheme,
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      backgroundColor: Colors.white,
+      foregroundColor: Colors.black,
+    ),
+  ),
+  outlinedButtonTheme: OutlinedButtonThemeData(
+    style: OutlinedButton.styleFrom(
+      foregroundColor: Colors.white,
+      side: const BorderSide(color: Colors.white),
+    ),
+  ),
 );

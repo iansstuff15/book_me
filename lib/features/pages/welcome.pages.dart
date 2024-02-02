d726598dd34132e7b467999aaa0ca65f52ba1528
Diriangen Lempira Powell <dlpowell35@gmail.com>
2024-02-02 17:33:37 +0800

    feat: updated welcome screen with disclaimer and URLs to site and email

--- lib/features/pages/welcome.pages.dart
+++ lib/features/pages/welcome.pages.dart
@@ -2,16 +2,32 @@ import 'package:book_me/config/app.config.dart';
 import 'package:book_me/config/font-weight.dart';
 import 'package:book_me/config/sizes.config.dart';
 import 'package:book_me/features/components/buttons/elevated-button.components.dart';
+import 'package:book_me/features/components/buttons/outlined-button.components.dart';
 import 'package:book_me/features/components/scaffold.components.dart';
+import 'package:book_me/util/launcher.dart';
+import 'package:book_me/util/os.dart';
 import 'package:flutter/material.dart';
 
 class WelcomePage extends StatelessWidget {
   static const String routeName = '/welcome';
-  const WelcomePage({super.key});
+
+  const WelcomePage({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
     return AppScaffold(
+      bottomWidget: Padding(
+          padding: EdgeInsets.only(
+              right: AppSizes.medium,
+              left: AppSizes.medium,
+              bottom: OS.isAndroid ? 0 : AppSizes.small),
+          child: AppElevatedButton(
+            block: true,
+            label: 'Get Started',
+            onPressed: () => {},
+          )),
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         const Spacer(),
@@ -60,14 +76,33 @@ class WelcomePage extends StatelessWidget {
           style: TextStyle(
               fontWeight: AppFontWeight.black, fontSize: AppSizes.large),
         ),
-        SizedBox(
+        const SizedBox(
           height: AppSizes.small,
         ),
+        const Text(
+            "For any inquiries or communication regarding Book Me or Diriangen Lempira Powell's Flutter app development skills, please feel free to reach out via email at dlpowell35@gmail.com. Your messages and questions are welcome, and Powell looks forward to engaging with you regarding his demo application and related expertise."),
         const Spacer(),
-        AppElevatedButton(
+        AppOutlineddButton(
+            onPressed: () {
+              Launch.email(email: 'dlpowell35@gmail.com', context: context);
+            },
+            label: 'Send me an email',
+            block: true),
+        AppOutlineddButton(
+          onPressed: () {
+            Launch.url(
+                url: 'https://github.com/iansstuff15/book_me',
+                context: context);
+          },
+          label: 'Check project code on GitHub',
           block: true,
-          label: 'Get Started',
-          onPressed: () => {},
+        ),
+        AppOutlineddButton(
+          onPressed: () {
+            Launch.url(url: 'https://by-ian.dev', context: context);
+          },
+          label: 'Check out Ian\'s website',
+          block: true,
         ),
         const Spacer(),
       ],
